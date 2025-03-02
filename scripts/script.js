1a42e954e25b9f08da4353b63fe7a58f67b1040d
unknown <mdadib300@gmail.com>
2025-03-02 22:52:11 +0600

    till Get element by tagname,id,class

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+// Get elements by Tag name
+const lists = document.getElementsByTagName('li');
+console.log(lists);
+
+// Get elements by ID
+console.log(document.getElementById('head').innerText);
+
+// Get elements by Class 
+console.log(document.getElementsByClassName('lists'));
+
+// Get elements By querySelector, querySelectorAll (Google it)
